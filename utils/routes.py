b6666f0674510f9dff14322e4234d9e48aa2ee75
Akib Rhast <akib.rhast@gmail.com>
2021-03-06 15:32:52 -0500

    Update routes.py

--- utils/routes.py
+++ utils/routes.py
@@ -2,5 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run()
